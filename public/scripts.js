f21e85a8ab5258bea366d16cc7b2c1d9f48f3b29
Dan Alvarez <danalvarez5280@gmail.com>
2017-11-02 15:43:18 -0600

    add local storage functionality, add order history post function to script.js

--- public/scripts.js
+++ public/scripts.js
@@ -4,6 +4,9 @@ const addToCart = (e) => {
 
   cart.push(data);
   localStorage.setItem('shoppingCart', JSON.stringify(cart));
+
+  $('.shopping-cart-items').remove();
+  getShoppingCart();
 };
 
 const displayCards = (array) => {
@@ -33,15 +36,34 @@ const getShoppingCart = () => {
   let cart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
   if(cart.length > 0) {
     return cart.forEach(item => {
-      $('.shopping-cart-page').append(
+      $('.shopping-cart-items').append(
       `<div class='cart-item'>
         <h5>Item Title:</h5>
         <p class='cart-item-title'>${item.title}</p>
         <h5>Item Price:</h5>
         <p class='cart-item-title'>$ ${item.price}</p>
+        <div class='purchase' data-title='${item.title}' data-price=${item.price}>Purchase</div>
       </div>`)
     })
   }
+};
+
+const purchaseItem = (e) => {
+  let data = e.target.dataset;
+  let order = {
+    item_title: data.title,
+    item_price: data.price
+  }
+  console.log('hi danman', data);
+  fetch('http://localhost:3001/api/v1/orders/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(order)
+  })
+  .then(data => data.json())
+  .then(thing => console.log('thing', thing))
 };
 
 const showCart = (e) => {
@@ -59,6 +81,7 @@ const showHistory = (e) => {
 
 $(document).ready(getInventory);
 $(document).ready(getShoppingCart);
+$('.shopping-cart-page').on('click', '.purchase', purchaseItem)
 $('.inventory').on('click', '.add-to-cart', addToCart);
 $('.shopping-cart').on('click', '.shopping-cart-button', showCart);
 $('.order-history').on('click', '.order-history-button', showHistory);
